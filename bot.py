464f1a732cecdce570d7a70028021540631632b0
Samuel Estrella <samuelmachado1@gmail.com>
2020-05-09 22:55:12 -0300

    Adding the required requirements file for publishing to Host Python

--- bot.py
+++ bot.py
@@ -1,4 +1,10 @@
-import telepot
+import telepot, time
 
 bot = telepot.Bot('1179774061:AAH3aHOypzml5UExYjf5-u4Qt9NDM6dvEIc')
-bot.sendMessage(669262997, "Deu bom camarada!!")
+
+counter = 0
+
+while True:
+    time.sleep(1)
+    bot.sendMessage(669262997, 'Mensagem número {}'.format(counter))
+    counter += 1
